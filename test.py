3ebed1d1b0a18e4f7be397e26337430420c533eb
Robert <rsmith8917@gmail.com>
2014-11-30 16:28:51 -0500

    indentation

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ import unittest
 class CalcTests(unittest.TestCase):
 
 	def test_calc_add(self):
-        calc = Calculator()
+		calc = Calculator()
         result = calc.add(2,2)
         self.assertEqual(4, result)
