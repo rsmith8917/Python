d4e812ec107917e9a78cdb0d79d240b9b5949233
Robert <rsmith8917@gmail.com>
2014-11-30 16:33:20 -0500

    more indents...

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ import unittest
 class CalcTests(unittest.TestCase):
 
 	def test_calc_add(self):
-        result = 2+2
-        self.assertEqual(4, result)
+		result=2+2
+		self.assertEqual(4, result)
