c433e9a13ec918712a1805014568a1eaad96460d
Robert <rsmith8917@gmail.com>
2014-11-30 16:31:47 -0500

    made simple test

--- test.py
+++ test.py
@@ -3,6 +3,5 @@ import unittest
 class CalcTests(unittest.TestCase):
 
 	def test_calc_add(self):
-		calc = Calculator()
-        result = calc.add(2,2)
+        result = 2+2
         self.assertEqual(4, result)
